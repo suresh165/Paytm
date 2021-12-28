394360758d70fcdf46335f9d060a44d4840394c3
Dell <csuresh073@gmail.com>
2021-12-28 16:45:45 +0530

    Initial commit

---
+++ app/src/main/java/suresh/chandra/paytmui/Mall.java
@@ -0,0 +1,14 @@
+package suresh.chandra.paytmui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mall extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mall);
+    }
+}
